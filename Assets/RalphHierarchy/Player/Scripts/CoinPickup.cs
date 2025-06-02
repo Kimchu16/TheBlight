a71bcdb7137afb9cc7dddb079a497c411c8508b3
RalphjCruz <ralphjcruz18@gmail.com>
2025-06-03 04:02:38 +0900

    Feature: InventoryImplementationAndConsumable

--- Assets/RalphHierarchy/Player/Scripts/CoinPickup.cs
+++ Assets/RalphHierarchy/Player/Scripts/CoinPickup.cs
@@ -2,17 +2,14 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    public int coinValue = 1; // how much this coin is worth (can expand later)
+    public int coinValue = 1;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Coin collected!");
-
-            // TODO: Add to player's coin count (optional, later)
-
-            Destroy(gameObject); // Destroy the coin after pickup
+            InventoryManager.Instance.AddCoin(coinValue);  // <-- this line MUST exist
+            Destroy(gameObject);
         }
     }
 }
