5e556fc30319d0bd0105011c31a3354f0d6e1859
Jana <janrheiasy@gmail.com>
2025-06-04 17:23:27 +0900

    finished timing attack sound, damage and anim

--- Assets/Scripts/Enemy/Hitboxes/BossGoblinAttackHitbox.cs
+++ Assets/Scripts/Enemy/Hitboxes/BossGoblinAttackHitbox.cs
@@ -1,33 +1,15 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BossGoblinAttackHitbox : EnemyAttackHitbox
 {
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            player = other.GetComponent<PlayerCharacter>();
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            if (player != null && other.gameObject == player.gameObject)
-            {
-                player = null;
-            }
-        }
-    }
-
     // Call this when boss swings
-    public new void DamagePlayer(float damageAmount)
+    public override void DamagePlayer(float damageAmount)
     {
         if (player != null)
         {
-            player.TakeDamage(damageAmount * 1.5f); // Boss does 50% more damage
+            player.TakeDamage(damageAmount * 1.8f); // Boss does 50% more damage
         }
     }
 }
